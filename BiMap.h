42ac1537f13517fca619e2f0df747b5765fa3de8
eric schkufza <eric.schkufza@gmail.com>
2011-12-08 00:50:12 -0800

    Realized that there's no reason to store duplicate copies in a BiMap. Added a few more methods; removed iterators for now.

--- BiMap.h
+++ BiMap.h
@@ -12,31 +12,33 @@ template <class Key, class Val>
 class BiMap
 {
   public:
-    unsigned int size() const;
-
     bool containsKey(const Key& key) const;
     bool containsVal(const Val& val) const;
 
     void clear();
 
+    bool empty() const;
+
+    void eraseKey(const Key& key);
+    void eraseVal(const Val& val);
+
+    const Val& getVal(const Key& key) const;
+    const Key& getKey(const Val& val) const;
+
     void insert(const Key& key, const Val& val);
 
-    Val getVal(const Key& key) const;
-    Key getKey(const Val& val) const;
-
-    typedef typename std::map<Key, Val>::const_iterator const_iterator;
-    const_iterator begin() const;
-    const_iterator end() const;
+    unsigned int size() const;
 
   private:
-    std::map<Key, Val> keyToVal_;
-    std::map<Val, Key> valToKey_;
+    std::map<const Key, const Val*> keyToVal_;
+    std::map<const Val, const Key*> valToKey_;
 };
 
 template <class Key, class Val>
-unsigned int BiMap<Key, Val>::size() const
+void BiMap<Key, Val>::clear()
 {
-  return keyToVal_.size();
+  keyToVal_.clear();
+  valToKey_.clear();
 }
 
 template <class Key, class Val>
@@ -52,10 +54,45 @@ bool BiMap<Key, Val>::containsVal(const Val& val) const
 }
 
 template <class Key, class Val>
-void BiMap<Key, Val>::clear()
+bool BiMap<Key, Val>::empty() const
 {
-  keyToVal_.clear();
-  valToKey_.clear();
+  return size() == 0;
+}
+
+template <class Key, class Val>
+void BiMap<Key, Val>::eraseKey(const Key& key)
+{
+  assert(containsKey(key) && "Unrecognized key!");
+
+  valToKey_.erase(getVal(key));
+  keyToVal_.erase(key);
+}
+
+template <class Key, class Val>
+void BiMap<Key, Val>::eraseVal(const Val& val) 
+{
+  assert(containsVal(val) && "Unrecognized val!");
+
+  keyToVal_.erase(getKey(val));
+  valToKey_.erase(val);
+}
+
+template <class Key, class Val>
+const Val& BiMap<Key, Val>::getVal(const Key& key) const
+{
+  assert(containsKey(key) && "Unrecognized key!");
+
+  auto itr = keyToVal_.find(key);
+  return *(itr->second);
+}
+
+template <class Key, class Val>
+const Key& BiMap<Key, Val>::getKey(const Val& val) const
+{
+  assert(containsVal(val) && "Unrecognized val!");
+
+  auto itr = valToKey_.find(val);
+  return *(itr->second);
 }
 
 template <class Key, class Val>
@@ -64,38 +101,20 @@ void BiMap<Key, Val>::insert(const Key& key, const Val& val)
   assert(!containsKey(key) && "Duplicate key!");
   assert(!containsVal(val) && "Duplicate val!");
 
-  keyToVal_[key] = val;
-  valToKey_[val] = key;
+  keyToVal_[key] = 0;
+  valToKey_[val] = 0;
+
+  auto keyitr = keyToVal_.find(key);
+  auto valitr = valToKey_.find(val);
+
+  keyToVal_[key] = &valitr->first;
+  valToKey_[val] = &keyitr->first;
 }
 
 template <class Key, class Val>
-Val BiMap<Key, Val>::getVal(const Key& key) const
+unsigned int BiMap<Key, Val>::size() const
 {
-  assert(containsKey(key) && "Unrecognized key!");
-
-  auto itr = keyToVal_.find(key);
-  return (*itr).second;
-}
-
-template <class Key, class Val>
-Key BiMap<Key, Val>::getKey(const Val& val) const
-{
-  assert(containsVal(val) && "Unrecognized val!");
-
-  auto itr = valToKey_.find(val);
-  return (*itr).second;
-}
-
-template <class Key, class Val>
-inline typename BiMap<Key, Val>::const_iterator BiMap<Key, Val>::begin() const
-{
-  return keyToVal_.begin();
-}
-
-template <class Key, class Val>
-inline typename BiMap<Key, Val>::const_iterator BiMap<Key, Val>::end() const
-{
-  return keyToVal_.end();
+  return keyToVal_.size();
 }
 
 }
