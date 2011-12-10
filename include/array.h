4b6f76b0e25c3b1dc20ae94da3d8fc4405a18d32
eric schkufza <eric.schkufza@gmail.com>
2011-12-10 13:57:09 -0800

    Really going for it.  Making a real-deal stl-compliant container.

--- include/array.h
+++ include/array.h
@@ -2,6 +2,7 @@
 #define ARRAY_H
 
 #include <cstddef>
+#include <stdexcept>
 
 namespace cpputil
 {
@@ -10,29 +11,111 @@ template <class T, int max>
 class array
 {
   public:
-    typedef T value_type;
 
-    typedef T* iterator;
-    typedef const T* const_iterator;
+    // Member Types
+    typedef T         value_type;
+    typedef ptrdiff_t size_type;
+    typedef ptrdiff_t difference_type;
+    typedef T*        iterator;
+    typedef const T*  const_iterator;
+    typedef T*        reverse_iterator;
+    typedef const T*  const_reverse_iterator;
+    typedef T&        reference;
+    typedef const T&  const_reference;
 
-    typedef T& reference;
-    typedef const T& const_reference;
+    // Constructors
+    array() { }
+    array(const T& t)
+    {
+      for ( auto i = 0; i < max; ++i )
+        xs[i] = t;
+    }
+    array(const array& rhs)
+    {
+      for ( auto i = 0; i < max; ++i )
+        xs[i] = rhs.x[i];
+    }
+    
+    // Assignments
+    array& operator=(const array& rhs)
+    {
+      if ( this != &rhs )
+        for ( auto i = 0; i < max; ++i )
+          xs[i] = rhs.xs[i];
+      return *this;
+    }
 
-    operator T*() { return v; }
+    // Conversion
+    operator T*() { return xs; }
 
-    reference operator[](ptrdiff_t i) { return v[i]; }
-    const_reference operator[](ptrdiff_t i) const { return v[i]; }
+    // Iterators
+    iterator begin() { return xs; }
+    iterator end() { return xs + max; }
+    reverse_iterator rbegin() { return xs + max - 1; }
+    reverse_iterator rend() { return xs - 1; }
 
-    iterator begin() { return v; }
-    const_iterator begin() const { return v; }
+    const_iterator begin() const { return xs; }
+    const_iterator end() const { return xs + max; }
+    const_reverse_iterator rbegin() const { return xs + max - 1; }
+    const_reverse_iterator rend() const { return xs - 1; }
+    
+    // Element Access
+    reference front() { return xs[0]; }
+    reference back() { return xs[max-1]; }
+    reference operator[](size_type i) { return xs[i]; }
+    reference at(size_type i) 
+    { 
+      if ( i < 0 || i >= max ) 
+        throw out_of_range("array::at"); 
+      return xs[i]; 
+    }
 
-    iterator end() { return v + max; }
-    const_iterator end() const { return v + max; }
+    const_reference front() const { return xs[0]; }
+    const_reference back() const { return xs[max-1]; }
+    const_reference operator[](size_type i) const { return xs[i]; }
+    const_reference at(size_type i) const 
+    { 
+      if ( i < 0 || i >= max ) 
+        throw out_of_range("array::at"); 
+      return xs[i]; 
+    }
 
-    size_t size() const { return max; }
+    // Other Operations
+    size_type size() const { return max; }
+    size_type max_size() const { return max; }
+    size_type capacity() const { return max; }
+    swap(array& rhs) 
+    { 
+      T* temp = xs;
+      xs = rhs.xs;
+      rhs.xs = temp;
+    }
+    bool operator==(const array& rhs) const 
+    { 
+      for ( auto i = 0; i < max; ++i )
+        if ( xs[i] != rhs.xs[i] )
+          return false;
+      return true;
+    }
+    bool operator!=(const array& rhs) const 
+    { 
+      for ( auto i = 0; i < max; ++i )
+        if ( xs[i] != rhs.xs[i] )
+          return true;
+      return false;
+    }
+    bool operator<(const array& rhs) const 
+    { 
+      for ( auto i = 0; i < max; ++i )
+        if ( xs[i] < rhs.xs[i] )
+          return true;
+        else if ( xs[i] > rhs.xs[i] )
+          return false;
+      return false;
+    }
 
   private:
-    T v[max];
+    T xs[max];
 };
 
 }
