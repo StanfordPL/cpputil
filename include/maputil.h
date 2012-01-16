0a6912096f2c8786442a8fefb2258dcc4b54dd37
eric schkufza <eric.schkufza@gmail.com>
2012-01-15 23:05:50 -0800

    Added key and value iterators to maputil. I *this* this is the best you can do. But I'd like to figure out how to get rid of the need for the explicit parameterization.

--- include/maputil.h
+++ include/maputil.h
@@ -27,6 +27,114 @@ inline typename _Associative::size_type assert_erase(_Associative& a, const type
   return a.erase(key);
 }
 
+template <typename _Associative>
+class value_iterator
+{
+  public:
+    value_iterator() = delete;
+
+    value_iterator(const typename _Associative::iterator& itr) : itr_(itr) {}
+
+    void operator++() { itr_++; }
+    typename _Associative::mapped_type& operator*() const { return itr_->second; }
+
+    bool operator==(const value_iterator& rhs) { return itr_ == rhs.itr_; }
+    bool operator!=(const value_iterator& rhs) { return itr_ != rhs.itr_; }
+
+  private:
+    typename _Associative::iterator itr_; 
+};
+
+template <typename _Associative>
+class const_key_iterator
+{
+  public:
+    const_key_iterator() = delete;
+
+    const_key_iterator(const typename _Associative::const_iterator& itr) : itr_(itr) {}
+
+    void operator++() { itr_++; }
+    const typename _Associative::key_type& operator*() const { return itr_->first; }
+
+    bool operator==(const const_key_iterator& rhs) { return itr_ == rhs.itr_; }
+    bool operator!=(const const_key_iterator& rhs) { return itr_ != rhs.itr_; }
+
+  private:
+    typename _Associative::const_iterator itr_; 
+};
+
+template <typename _Associative>
+class const_value_iterator
+{
+  public:
+    const_value_iterator() = delete;
+
+    const_value_iterator(const typename _Associative::const_iterator& itr) : itr_(itr) {}
+
+    void operator++() { itr_++; }
+    const typename _Associative::mapped_type& operator*() const { return itr_->second; }
+
+    bool operator==(const const_value_iterator& rhs) { return itr_ == rhs.itr_; }
+    bool operator!=(const const_value_iterator& rhs) { return itr_ != rhs.itr_; }
+
+  private:
+    typename _Associative::const_iterator itr_; 
+};
+
+template <typename _Associative>
+class reverse_value_iterator
+{
+  public:
+    reverse_value_iterator() = delete;
+
+    reverse_value_iterator(const typename _Associative::reverse_iterator& itr) : itr_(itr) {}
+
+    void operator++() { itr_++; }
+    typename _Associative::mapped_type& operator*() const { return itr_->second; }
+
+    bool operator==(const reverse_value_iterator& rhs) { return itr_ == rhs.itr_; }
+    bool operator!=(const reverse_value_iterator& rhs) { return itr_ != rhs.itr_; }
+
+  private:
+    typename _Associative::reverse_iterator itr_; 
+};
+
+template <typename _Associative>
+class const_reverse_key_iterator
+{
+  public:
+    const_reverse_key_iterator() = delete;
+
+    const_reverse_key_iterator(const typename _Associative::const_reverse_iterator& itr) : itr_(itr) {}
+
+    void operator++() { itr_++; }
+    const typename _Associative::key_type& operator*() const { return itr_->first; }
+
+    bool operator==(const const_reverse_key_iterator& rhs) { return itr_ == rhs.itr_; }
+    bool operator!=(const const_reverse_key_iterator& rhs) { return itr_ != rhs.itr_; }
+
+  private:
+    typename _Associative::const_reverse_iterator itr_; 
+};
+
+template <typename _Associative>
+class const_reverse_value_iterator
+{
+  public:
+    const_reverse_value_iterator() = delete;
+
+    const_reverse_value_iterator(const typename _Associative::const_reverse_iterator& itr) : itr_(itr) {}
+
+    void operator++() { itr_++; }
+    const typename _Associative::mapped_type& operator*() const { return itr_->second; }
+
+    bool operator==(const const_reverse_value_iterator& rhs) { return itr_ == rhs.itr_; }
+    bool operator!=(const const_reverse_value_iterator& rhs) { return itr_ != rhs.itr_; }
+
+  private:
+    typename _Associative::const_reverse_iterator itr_; 
+};
+
 }
 
 #endif
--- test/maputil.cc
+++ test/maputil.cc
@@ -17,6 +17,28 @@ int main()
   for ( auto i = 0; i < 10; ++i )
     m[i] = i;
 
+  // These should all print 0 .. 9
+  for ( auto i = const_key_iterator<map<const int, int>>(m.begin()), ie = const_key_iterator<map<const int, int>>(m.end()); i != ie; ++i )
+    cout << *i << " ";
+  cout << endl;
+  for ( auto i = value_iterator<map<const int, int>>(m.begin()), ie = value_iterator<map<const int, int>>(m.end()); i != ie; ++i )
+    cout << *i << " ";
+  cout << endl;
+  for ( auto i = const_value_iterator<map<const int, int>>(m.begin()), ie = const_value_iterator<map<const int, int>>(m.end()); i != ie; ++i )
+    cout << *i << " ";
+  cout << endl;
+
+  // These should all print 9 .. 0
+  for ( auto i = const_reverse_key_iterator<map<const int, int>>(m.rbegin()), ie = const_reverse_key_iterator<map<const int, int>>(m.rend()); i != ie; ++i )
+    cout << *i << " ";
+  cout << endl;
+  for ( auto i = reverse_value_iterator<map<const int, int>>(m.rbegin()), ie = reverse_value_iterator<map<const int, int>>(m.rend()); i != ie; ++i )
+    cout << *i << " ";
+  cout << endl;
+  for ( auto i = const_reverse_value_iterator<map<const int, int>>(m.rbegin()), ie = const_reverse_value_iterator<map<const int, int>>(m.rend()); i != ie; ++i )
+    cout << *i << " ";
+  cout << endl;
+
   // Both should print 5
   cout << assert_at(m, 5) << endl;
   foo(m);
