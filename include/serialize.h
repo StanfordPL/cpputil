f43654403a2b8c0aa45ca2ed742bb52cc7b8a200
eric schkufza <eric.schkufza@gmail.com>
2012-01-16 17:44:53 -0800

    The end of a long infuriating odyssey through why you can't have partial functiont template specialization.

--- include/serialize.h
+++ include/serialize.h
@@ -15,193 +15,255 @@ namespace cpputil
 {
 
 template <typename _T>
-void serialize(std::ostream& os, const _T& t, char delim = '"')
+struct Serializer
 {
-  os << t;
-}
+  static void serialize(std::ostream& os, const _T& t, char delim = '"')
+  {
+    os << t;
+  }
+};
+
+template <typename _Char, typename _Traits>
+struct Serializer<std::basic_string<_Char, _Traits>>
+{
+  static void serialize(std::ostream& os, const std::basic_string<_Char, _Traits>& s, char delim = '"') 
+  { 
+    os << delim << s << delim; 
+  }
+};
 
 template <typename _T>
-void serialize(std::ostream& os, const std::deque<_T>& ts, char delim = '"')
+struct Serializer<std::deque<_T>>
 {
-  os << ts.size() << " ";
-  for ( auto t : ts )
+  static void serialize(std::ostream& os, const std::deque<_T>& ts, char delim = '"')
   {
-    serialize(os, t, delim);
-    os << " ";
+    os << ts.size() << " ";
+    for ( auto t : ts )
+    {
+      Serializer<_T>::serialize(os, t, delim);
+      os << " ";
+    }
   }
-}
+};
 
 template <typename _T>
-void serialize(std::ostream& os, const std::list<_T>& ts, char delim = '"')
+struct Serializer<std::list<_T>>
 {
-  os << ts.size() << " ";
-  for ( auto t : ts )
+  static void serialize(std::ostream& os, const std::list<_T>& ts, char delim = '"')
   {
-    serialize(os, t, delim);
-    os << " ";
+    os << ts.size() << " ";
+    for ( auto t : ts )
+    {
+      Serializer<_T>::serialize(os, t, delim);
+      os << " ";
+    }
   }
-}
+};
+
+template <typename _T>
+struct Serializer<std::set<_T>>
+{
+  static void serialize(std::ostream& os, const std::set<_T>& ts, char delim = '"')
+  {
+    os << ts.size() << " ";
+    for ( auto t : ts )
+    {
+      Serializer<_T>::serialize(os, t, delim);
+      os << " ";
+    }
+  }
+};
 
 template <typename _Key, typename _Val>
-void serialize(std::ostream& os, const std::map<_Key, _Val>& m, char delim = '"')
+struct Serializer<std::map<_Key, _Val>>
 {
-  os << m.size() << " ";
-  for ( auto p : m )
+  static void serialize(std::ostream& os, const std::map<_Key, _Val>& m, char delim = '"')
   {
-    serialize(os, p.first, delim);
-    os << " ";
-    serialize(os, p.second, delim);
-    os << " ";
+    os << m.size() << " ";
+    for ( auto p : m )
+    {
+      Serializer<_Key>::serialize(os, p.first, delim);
+      os << " ";
+      Serializer<_Val>::serialize(os, p.second, delim);
+      os << " ";
+    }
   }
+};
+
+template <typename _T>
+struct Serializer<tokenizer<_T>>
+{
+  static void serialize(std::ostream& os, const tokenizer<_T>& ts, char delim = '"')
+  {
+    os << ts.size() << " ";
+    for ( auto t : ts )
+    {
+      Serializer<_T>::serialize(os, t, delim);
+      os << " ";
+    }
+  }
+};
+
+template <typename _T>
+struct Serializer<std::vector<_T>>
+{
+  static void serialize(std::ostream& os, const std::vector<_T>& ts, char delim = '"')
+  {
+    os << ts.size() << " ";
+    for ( auto t : ts )
+    {
+      Serializer<_T>::serialize(os, t, delim);
+      os << " ";
+    }
+  }
+};
+
+template <typename _T>
+void serialize(std::ostream& os, const _T& t, char delim = '"')
+{
+  Serializer<_T>::serialize(os, t, delim);
 }
 
 template <typename _T>
-void serialize(std::ostream& os, const std::set<_T>& ts, char delim = '"')
+struct Deserializer
 {
-  os << ts.size() << " ";
-  for ( auto t : ts )
+  static void deserialize(std::istream& is, _T& t, char delim = '"')
   {
-    serialize(os, t, delim);
-    os << " ";
+    is >> t;
   }
-}
+};
 
-void serialize(std::ostream& os, const std::string& s, char delim = '"')
+template <typename _Char, typename _Traits>
+struct Deserializer<std::basic_string<_Char, _Traits>>
 {
-  os << delim << s << delim;
-}
-
-template <typename _T>
-void serialize(std::ostream& os, const tokenizer<_T>& ts, char delim = '"')
-{
-  os << ts.size() << " ";
-  for ( auto t : ts )
+  static void deserialize(std::istream& is, std::basic_string<_Char, _Traits>& s, char delim = '"')
   {
-    serialize(os, t, delim);
-    os << " ";
+    getline(is, s, delim);
+    getline(is, s, delim);
   }
-}
+};
 
 template <typename _T>
-void serialize(std::ostream& os, const std::vector<_T>& ts, char delim = '"')
+struct Deserializer<std::deque<_T>>
 {
-  os << ts.size() << " ";
-  for ( auto t : ts )
+  static void deserialize(std::istream& is, std::deque<_T>& ts, char delim = '"')
   {
-    serialize(os, t, delim);
-    os << " ";
+    ts.clear();
+
+    typename std::deque<_T>::size_type size;
+    is >> size;
+
+    _T t;
+    for ( auto i = 0; i < size; ++i )
+    {
+      Deserializer<_T>::deserialize(is, t, delim);
+      ts.emplace_back(t);
+    }
   }
-}
+};
+
+template <typename _T>
+struct Deserializer<std::list<_T>>
+{
+  static void deserialize(std::istream& is, std::list<_T>& ts, char delim = '"')
+  {
+    ts.clear();
+
+    typename std::list<_T>::size_type size;
+    is >> size;
+
+    _T t;
+    for ( auto i = 0; i < size; ++i )
+    {
+      Deserializer<_T>::deserialize(is, t, delim);
+      ts.emplace_back(t);
+    }
+  }
+};
+
+template <typename _Key, typename _Val>
+struct Deserializer<std::map<_Key, _Val>>
+{
+  static void deserialize(std::istream& is, std::map<_Key, _Val>& m, char delim = '"')
+  {
+    m.clear();
+
+    typename std::map<_Key, _Val>::size_type size;
+    is >> size;
+
+    _Key key;
+    _Val val;
+    for ( auto i = 0; i < size; ++i )
+    {
+      Deserializer<_Key>::deserialize(is, key, delim);
+      Deserializer<_Val>::deserialize(is, val, delim);
+      m[key] = val;
+    }
+  }
+};
+
+template <typename _T>
+struct Deserializer<std::set<_T>>
+{
+  static void deserialize(std::istream& is, std::set<_T>& ts, char delim = '"')
+  {
+    ts.clear();
+
+    typename std::set<_T>::size_type size;
+    is >> size;
+
+    _T t;
+    for ( auto i = 0; i < size; ++i )
+    {
+      Deserializer<_T>::deserialize(t);
+      ts.insert(t);
+    }
+  }
+};
+
+template <typename _T>
+struct Deserializer<tokenizer<_T>>
+{
+  static void deserialize(std::istream& is, tokenizer<_T>& ts, char delim = '"')
+  {
+    ts.clear();
+
+    typename tokenizer<_T>::size_type size;
+    is >> size;
+
+    _T t;
+    for ( auto i = 0; i < size; ++i )
+    {
+      Deserializer<_T>::deserialize(is, t, delim);
+      ts.tokenize(t);
+    }
+  }
+};
+
+template <typename _T>
+struct Deserializer<std::vector<_T>>
+{
+  static void deserialize(std::istream& is, std::vector<_T>& ts, char delim = '"')
+  {
+    ts.clear();
+
+    typename std::vector<_T>::size_type size;
+    is >> size;
+    ts.reserve(size);
+
+    _T t;
+    for ( auto i = 0; i < size; ++i )
+    {
+      Deserializer<_T>::deserialize(is, t, delim);
+      ts.emplace_back(t);
+    }
+  }
+};
 
 template <typename _T>
 void deserialize(std::istream& is, _T& t, char delim = '"')
 {
-  is >> t;
-}
-
-template <typename _T>
-void deserialize(std::istream& is, std::deque<_T>& ts, char delim = '"')
-{
-  ts.clear();
-
-  typename std::deque<_T>::size_type size;
-  is >> size;
-  
-  _T t;
-  for ( auto i = 0; i < size; ++i )
-  {
-    deserialize(is, t, delim);
-    ts.emplace_back(t);
-  }
-}
-
-template <typename _T>
-void deserialize(std::istream& is, std::list<_T>& ts, char delim = '"')
-{
-  ts.clear();
-
-  typename std::list<_T>::size_type size;
-  is >> size;
-
-  _T t;
-  for ( auto i = 0; i < size; ++i )
-  {
-    deserialize(is, t, delim);
-    ts.emplace_back(t);
-  }
-}
-
-template <typename _Key, typename _Val>
-void deserialize(std::istream& is, std::map<_Key, _Val>& m, char delim = '"')
-{
-  m.clear();
-
-  typename std::map<_Key, _Val>::size_type size;
-  is >> size;
-
-  _Key key;
-  _Val val;
-  for ( auto i = 0; i < size; ++i )
-  {
-    deserialize(is, key, delim);
-    deserialize(is, val, delim);
-    m[key] = val;
-  }
-}
-
-template <typename _T>
-void deserialize(std::istream& is, std::set<_T>& ts, char delim = '"')
-{
-  ts.clear();
-
-  typename std::set<_T>::size_type size;
-  is >> size;
-
-  _T t;
-  for ( auto i = 0; i < size; ++i )
-  {
-    deserialize(t);
-    ts.insert(t);
-  }
-}
-
-void deserialize(std::istream& is, std::string& s, char delim = '"')
-{
-  getline(is, s, delim);
-  getline(is, s, delim);
-}
-
-template <typename _T>
-void deserialize(std::istream& is, tokenizer<_T>& ts, char delim = '"')
-{
-  ts.clear();
-
-  typename tokenizer<_T>::size_type size;
-  is >> size;
-  
-  _T t;
-  for ( auto i = 0; i < size; ++i )
-  {
-    deserialize(is, t, delim);
-    ts.tokenize(t);
-  }
-}
-
-template <typename _T>
-void deserialize(std::istream& is, std::vector<_T>& ts, char delim = '"')
-{
-  ts.clear();
-
-  typename std::vector<_T>::size_type size;
-  is >> size;
-  ts.reserve(size);
-
-  _T t;
-  for ( auto i = 0; i < size; ++i )
-  {
-    deserialize(is, t, delim);
-    ts.emplace_back(t);
-  }
+  Deserializer<_T>::deserialize(is, t, delim);
 }
 
 } 
--- test/serialize.cc
+++ test/serialize.cc
@@ -75,5 +75,25 @@ int main()
   tests(v);
   testt(t);
 
+  set<string> ss;
+  ss.insert("[Hello]");
+  ss.insert("[World!]");
+  ss.insert("[foo]");
+  serialize(cout, ss);
+  cout << endl;
+
+  map<int, set<string>> mss;
+  mss[0].insert("Hello");
+  mss[0].insert("World!");
+  mss[1].insert("foo");
+  serialize(cout, mss);
+  cout << endl;
+
+  set<map<int,int>> sm;
+  sm.insert(m);
+  serialize(cout, sm);
+  cout << endl;
+
+
   return 0;
 }
