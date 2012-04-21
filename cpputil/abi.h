12f7fc9bbc64010ee5bf5eb0cb17ae412b797840
eric schkufza <eric.schkufza@gmail.com>
2012-04-20 19:43:11 -0700

    Wrapper around abi utilities.

---
+++ cpputil/abi.h
@@ -0,0 +1,25 @@
+#ifndef CPPUTIL_ABI_H
+#define CPPUTIL_ABI_H
+
+#include <string>
+
+#include <cxxabi.h>
+
+namespace cpputil
+{
+
+std::string unmangle(const char* mangled)
+{
+  int status;
+  char* original = abi::__cxa_demangle(mangled, 0, 0, &status);
+  
+  std::string unmangled = original != 0 ? original : mangled;
+  if ( original ) 
+    free(original);
+
+  return unmangled;
+}
+
+}
+
+#endif
