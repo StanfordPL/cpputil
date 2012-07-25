d666b519b6eee61059a15a832c3549b7046e7251
eric <eschkufz@kokako.(none)>
2012-07-24 18:57:53 -0700

    Revamped the stream directory. Got rid of the comments... so heavy... and I still change this, so who knows.

---
+++ cpputil/stream/nullstream.h
@@ -0,0 +1,46 @@
+#ifndef CPPUTIL_STREAM_NULLSTREAM_H
+#define CPPUTIL_STREAM_NULLSTREAM_H
+
+#include <iostream>
+#include <streambuf>
+
+namespace cpputil
+{
+
+template <typename Ch, typename Tr>
+class basic_nullbuf : public std::basic_streambuf<Ch, Tr>
+{
+	public:
+
+	  typedef typename std::basic_streambuf<Ch, Tr>::traits_type traits_type; 
+  	typedef typename std::basic_streambuf<Ch, Tr>::int_type    int_type;    
+
+		inline explicit basic_nullbuf() : std::basic_streambuf<Ch, Tr>() { }
+		inline virtual ~basic_nullbuf() { }
+
+	protected:
+
+    inline virtual int sync() { return 0; }
+    inline virtual int_type overflow(int_type c = traits_type::eof()) { return c; }
+};
+
+template <typename Ch, typename Tr>
+class basic_onullstream : public std::basic_ostream<Ch, Tr>
+{
+	public:
+
+		inline explicit basic_onullstream(std::basic_ostream<Ch, Tr>& is) : std::basic_ostream<Ch, Tr>(&buf_), buf_() { }	
+		inline explicit basic_onullstream(std::basic_streambuf<Ch, Tr>* sb) : std::basic_ostream<Ch, Tr>(&buf_), buf_() { }
+		inline virtual ~basic_onullstream() { }
+
+	private:
+
+		basic_nullbuf<Ch, Tr> buf_;		
+};
+
+typedef basic_onullstream<char, std::char_traits<char>> onullstream;
+typedef basic_onullstream<wchar_t, std::char_traits<wchar_t>> wonullstream;
+
+}
+
+#endif
