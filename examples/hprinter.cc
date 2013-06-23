67ff76585a03d26e0539138ec46aec2de140c036
eric <eric.schkufza@gmail.com>
2013-06-23 00:34:02 -0700

    Done with hprinter class.

---
+++ examples/hprinter.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+
+#include "src/hprinter.h"
+
+using namespace cpputil;
+using namespace std;
+
+struct Double {
+	string x;
+	string y;
+};
+
+struct Triple {
+	int x;
+	int y;
+	int z;
+};
+
+struct Sext {
+	Triple t1;
+	Triple t2;
+};
+
+ostream& operator<<(ostream& os, const Double& d) {
+	os << d.x << endl;
+	os << d.y;
+	return os;
+}
+
+ostream& operator<<(ostream& os, const Triple& t) {
+	os << t.x << endl;
+	os << t.y << endl;
+	os << t.z;
+	return os;
+}
+
+ostream& operator<<(ostream& os, const Sext& s) {
+	os << s.t1 << endl;
+	os << s.t2;
+	return os;
+}
+
+int main() {
+	HPrinter hp(cout);
+	
+	Double d{"Hello", "World!!!"};
+	Triple t{1,2,3};
+	Sext s{{1,2,3},{4,5,6}};
+
+	hp << t << s << d << s << t << endline;
+
+	return 0;
+}
--- src/hprinter.h
+++ src/hprinter.h
@@ -1,17 +1,116 @@
 #ifndef CPPUTIL_SRC_HPRINTER_H
 #define CPPUTIL_SRC_HPRINTER_H
 
+#include <algorithm>
 #include <initializer_list>
 #include <iostream>
 #include <sstream>
+#include <vector>
 
 namespace cpputil {
 
 class HPrinter {
 	public:
-		static void print(std::initializer_list<T&
+		HPrinter(std::ostream& os);
 
+		HPrinter& set_vspace(size_t vspace);
+
+		template <typename T>
+		HPrinter& operator<<(const T& t);
+		HPrinter& operator<<(HPrinter& (*fxn)(HPrinter&));
+		HPrinter& flush();
+
+	private:
+		typedef std::vector<std::string> col_type;
+
+		std::ostream& os_;
+		std::vector<col_type> cols_;
+		size_t vspace_;
 };
+
+HPrinter& endline(HPrinter& hp);
+
+inline HPrinter::HPrinter(std::ostream& os) :
+		os_(os) {
+	set_vspace(1);
+}
+
+inline HPrinter& HPrinter::set_vspace(size_t vspace) {
+	vspace_ = vspace;
+}
+
+template <typename T>
+inline HPrinter& HPrinter::operator<<(const T& t) {
+	std::ostringstream oss;
+	oss << t;
+
+	std::istringstream iss(oss.str());
+	std::string s;
+
+	col_type col;
+	while ( getline(iss, s) )
+		col.push_back(s);
+	cols_.push_back(col);
+
+	return *this;
+}
+
+inline HPrinter& HPrinter::operator<<(HPrinter& (*fxn)(HPrinter&)) {
+	return fxn(*this);
+}
+
+inline HPrinter& HPrinter::flush() {
+	if ( cols_.empty() ) {
+		os_ << std::endl;
+		return *this;
+	}
+
+	std::vector<size_t> col_widths;
+	for ( const auto& col : cols_ ) {
+		auto comp = [](const std::string& s1, const std::string& s2) { 
+			return s1.length() < s2.length();
+		};
+		const auto itr = std::max_element(col.begin(), col.end(), comp);
+		col_widths.push_back(itr->length());
+	}
+
+	auto comp = [](const col_type& col1, const col_type& col2) {
+	 	return col1.size() < col2.size();
+	};
+	const auto itr = std::max_element(cols_.begin(), cols_.end(), comp);
+	const auto max_height = itr->size();
+
+	for ( size_t i = 0, ie = cols_.size(); i < ie; ++i ) {
+		auto& col = cols_[i];
+		std::string blank = "";
+		for ( size_t j = 0, je = col_widths[i]; j < je; ++j )
+			blank += " ";
+
+		size_t j = 0;
+		for ( size_t je = col.size(); j < je; ++j ) 
+			while ( col[j].length() < col_widths[i] )
+				col[j] += " ";
+		for ( ; j < max_height; ++j )
+			col.push_back(blank);
+	}
+
+	for ( size_t i = 0; i < max_height; ++i ) {
+		for ( size_t j = 0, je = cols_.size(); j < je; ++j ) {
+			os_ << cols_[j][i];
+			for ( size_t k = 0; k < vspace_; ++k )
+				os_ << " ";
+		}
+		os_ << std::endl;
+	}
+
+	cols_.clear();
+
+	return *this;
+}
+
+inline HPrinter& endline(HPrinter& hp) {
+	return hp.flush();
+}
 
 } // namespace cpputil
 
