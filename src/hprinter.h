00209c52f5aa0479cd137f64011600388ace876d
eric schkufza <eric.schkufza@gmail.com>
2013-06-22 23:19:59 -0700

    New horizontal printer file.

---
+++ src/hprinter.h
@@ -0,0 +1,18 @@
+#ifndef CPPUTIL_SRC_HPRINTER_H
+#define CPPUTIL_SRC_HPRINTER_H
+
+#include <initializer_list>
+#include <iostream>
+#include <sstream>
+
+namespace cpputil {
+
+class HPrinter {
+	public:
+		static void print(std::initializer_list<T&
+
+};
+
+} // namespace cpputil
+
+#endif
