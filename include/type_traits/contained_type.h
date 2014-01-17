82656fdc199f421c1772e996e0fdd42b97be0c11
eric schkufza <eric.schkufza@gmail.com>
2014-01-17 15:29:32 -0800

    Not sure why I'm adding this. Find out the native type inside an stl sequence.

---
+++ include/type_traits/contained_type.h
@@ -0,0 +1,18 @@
+#include <type_traits>
+
+#include "include/type_traits/is_stl_sequence.h"
+
+
+namespace cpputil {
+
+template <typename T, typename Ignore = void>
+struct contained_type {
+	typedef T type;
+};
+
+template <typename T>
+struct contained_type<T, typename std::enable_if<is_stl_sequence<T>::value>::type> {
+	typedef typename contained_type<typename T::value_type>::type type;
+};
+
+} // namespace cpputil
