1530c38fb28a1b7e551aff221b31518acbd93d36
eric <eric.schkufza@gmail.com>
2014-01-17 19:58:20 -0800

    Examples for containers.

--- include/container/bijection.h
+++ include/container/bijection.h
@@ -1,18 +1,16 @@
-/*
-Copyright 2013 eric schkufza
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+// Copyright 2014 eric schkufza
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #ifndef CPPUTIL_INCLUDE_CONTAINER_BIJECTION_H
 #define CPPUTIL_INCLUDE_CONTAINER_BIJECTION_H
@@ -30,10 +28,10 @@ class Bijection {
     typedef const domain_type& const_domain_reference;
     typedef R range_type;
     typedef const range_type& const_range_reference;
-    typedef typename DMap::map_type::const_iterator const_iterator;
-    typedef typename DMap::map_type::value_type value_type;
+    typedef typename DMap::const_iterator const_iterator;
+    typedef typename DMap::value_type value_type;
     typedef const value_type& const_reference;
-    typedef typename DMap::map_type::size_type size_type;
+    typedef typename DMap::size_type size_type;
 
     const_iterator begin() const {
       return d2r_.begin(); 
