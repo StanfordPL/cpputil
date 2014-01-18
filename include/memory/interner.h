eb205fd9f012ea2ac670cfd539f95739e7e300c8
eric <eric.schkufza@gmail.com>
2014-01-17 19:50:42 -0800

    Interner example.

--- include/memory/interner.h
+++ include/memory/interner.h
@@ -1,18 +1,16 @@
-/*
-Copyright 2013 eric schkufza
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+// Copyright 2014 eric schkufza
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #ifndef CPPUTIL_INCLUDE_MEMORY_INTERNER_H
 #define CPPUTIL_INCLUDE_MEMORY_INTERNER_H
@@ -32,12 +30,6 @@ class Interner {
 
     const_reference intern(const_reference t) {
       const auto res = vals_.insert(t);
-      return *(res.first);
-    }
-
-    template <typename... T>
-    const_reference intern(T&&... t) {
-      const auto res = vals_.emplace(std::forward(t));
       return *(res.first);
     }
 
