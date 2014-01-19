f7233264708365c567a1a69e1071add35598814e
eric <eric.schkufza@gmail.com>
2014-01-19 10:23:02 -0800

    astyle

--- examples/serialize/text.cc
+++ examples/serialize/text.cc
@@ -25,71 +25,71 @@ using namespace cpputil;
 using namespace std;
 
 struct C {
-	int x;
-	char c;
-	double d;
-	string s;
+  int x;
+  char c;
+  double d;
+  string s;
 };
 
 template <char Open, char Close, char Quote>
 struct TextWriter<C, Open, Close, Quote> {
-	void operator()(ostream& os, const C& c) {
-		os << Open;
-		TextWriter<int,Open,Close,Quote>()(os, c.x);
-	 	os << " ";
-		TextWriter<char,Open,Close,Quote>()(os, c.c);
-	 	os << " ";
-		TextWriter<double,Open,Close,Quote>()(os, c.d);
-	 	os << " ";
-		TextWriter<string,Open,Close,Quote>()(os, c.s);
-	 	os << Close;
-	}
+  void operator()(ostream& os, const C& c) {
+    os << Open;
+    TextWriter<int, Open, Close, Quote>()(os, c.x);
+    os << " ";
+    TextWriter<char, Open, Close, Quote>()(os, c.c);
+    os << " ";
+    TextWriter<double, Open, Close, Quote>()(os, c.d);
+    os << " ";
+    TextWriter<string, Open, Close, Quote>()(os, c.s);
+    os << Close;
+  }
 };
 
 template <char Open, char Close, char Quote>
 struct TextReader<C, Open, Close, Quote> {
-	void operator()(istream& is, C& c) {
-		is.get();
-		TextReader<int,Open,Close,Quote>()(is, c.x);
-		is.get();
-		TextReader<char,Open,Close,Quote>()(is, c.c);
-		is.get();
-		TextReader<double,Open,Close,Quote>()(is, c.d);
-		is.get();
-		TextReader<string,Open,Close,Quote>()(is, c.s);
-		is.get();
-	}
+  void operator()(istream& is, C& c) {
+    is.get();
+    TextReader<int, Open, Close, Quote>()(is, c.x);
+    is.get();
+    TextReader<char, Open, Close, Quote>()(is, c.c);
+    is.get();
+    TextReader<double, Open, Close, Quote>()(is, c.d);
+    is.get();
+    TextReader<string, Open, Close, Quote>()(is, c.s);
+    is.get();
+  }
 };
 
 template <typename T>
 ostream& operator<<(ostream& os, const T& t) {
-	TextWriter<T> tw;
-	tw(os, t);
-	return os;
+  TextWriter<T> tw;
+  tw(os, t);
+  return os;
 }
 
 template <typename T>
 istream& operator>>(istream& is, T& t) {
-	TextReader<T> tr;
-	tr(is, t);
-	return is;
+  TextReader<T> tr;
+  tr(is, t);
+  return is;
 }
 
 int main() {
-	C c {1,'c',2.0,"Hello"};
-	vector<int> v {1,2};
-	list<decltype(v)> l {v,v};
-	map<decltype(l), vector<C>> m {{l,{c}}};
-	pair<decltype(m), short> p {m,3};
+  C c {1, 'c', 2.0, "Hello"};
+  vector<int> v {1, 2};
+  list<decltype(v)> l {v, v};
+  map<decltype(l), vector<C>> m {{l, {c}}};
+  pair<decltype(m), short> p {m, 3};
 
-	stringstream ss;
-	ss << p << endl;
+  stringstream ss;
+  ss << p << endl;
 
-	decltype(p) p2;
-	ss >> p2;
+  decltype(p) p2;
+  ss >> p2;
 
-	cout << p << endl;
-	cout << p2 << endl;
+  cout << p << endl;
+  cout << p2 << endl;
 
-	return 0;
+  return 0;
 };
--- include/serialize/text.h
+++ include/serialize/text.h
@@ -30,7 +30,7 @@ struct TextReader;
 
 template <typename T, char Open, char Close, char Quote>
 struct TextReader < T, Open, Close, Quote,
-    typename std::enable_if < std::is_scalar<T>::value && !std::is_enum<T>::value >::type > {
+    typename std::enable_if < std::is_scalar<T>::value&&  !std::is_enum<T>::value >::type > {
   void operator()(std::istream& is, T& t) const {
     is >> t;
   }
@@ -38,10 +38,10 @@ struct TextReader < T, Open, Close, Quote,
 
 template <typename T, char Open, char Close, char Quote>
 struct TextReader < T, Open, Close, Quote,
-    typename std::enable_if < std::is_scalar<T>::value && std::is_enum<T>::value >::type > {
+    typename std::enable_if < std::is_scalar<T>::value&&  std::is_enum<T>::value >::type > {
   void operator()(std::istream& is, T& t) const {
-		typename std::underlying_type<T>::type v;
-		is >> v;
+    typename std::underlying_type<T>::type v;
+    is >> v;
     t = (T)v;;
   }
 };
@@ -49,35 +49,35 @@ struct TextReader < T, Open, Close, Quote,
 template <char Open, char Close, char Quote>
 struct TextReader<std::string, Open, Close, Quote, void> {
   void operator()(std::istream& is, std::string& s) const {
-		if ( is.get() != Quote ) {
-			is.setstate(std::ios::failbit);
-		}
-		std::getline(is, s, Quote);
-	}
+    if (is.get() != Quote) {
+      is.setstate(std::ios::failbit);
+    }
+    std::getline(is, s, Quote);
+  }
 };
 
 template <typename T1, typename T2, char Open, char Close, char Quote>
 struct TextReader<std::pair<T1, T2>, Open, Close, Quote, void> {
   void operator()(std::istream& is, std::pair<T1, T2>& p) const {
-		auto die_unless = [](std::istream& is, char c) { 
-			if ( is.get() != c ) {
-				is.setstate(std::ios::failbit);
-			}
-		};
+    auto die_unless = [](std::istream & is, char c) {
+      if (is.get() != c) {
+        is.setstate(std::ios::failbit);
+      }
+    };
 
-		die_unless(is, Open);
-		die_unless(is, ' ');
+    die_unless(is, Open);
+    die_unless(is, ' ');
 
     TextReader<T1, Open, Close, Quote> r1;
     r1(is, p.first);
 
-		die_unless(is, ' ');
+    die_unless(is, ' ');
 
     TextReader<T2, Open, Close, Quote> r2;
     r2(is, p.second);
 
-		die_unless(is, ' ');
-		die_unless(is, Close);
+    die_unless(is, ' ');
+    die_unless(is, Close);
   }
 };
 
@@ -85,14 +85,14 @@ template <typename T, char Open, char Close, char Quote>
 struct TextReader<T, Open, Close, Quote,
     typename std::enable_if<is_stl_sequence<T>::value>::type> {
   void operator()(std::istream& is, T& t) const {
-		auto die_unless = [](std::istream& is, char c) { 
-			if ( is.get() != c ) {
-				is.setstate(std::ios::failbit);
-			}
-		};
+    auto die_unless = [](std::istream & is, char c) {
+      if (is.get() != c) {
+        is.setstate(std::ios::failbit);
+      }
+    };
 
-		die_unless(is, Open);
-		die_unless(is, ' ');
+    die_unless(is, Open);
+    die_unless(is, ' ');
 
     t.clear();
     while (is.peek() != Close) {
@@ -101,9 +101,9 @@ struct TextReader<T, Open, Close, Quote,
       r(is, v);
       t.push_back(v);
 
-			die_unless(is, ' ');
+      die_unless(is, ' ');
     }
-		die_unless(is, Close);
+    die_unless(is, Close);
   }
 };
 
@@ -111,14 +111,14 @@ template <typename T, char Open, char Close, char Quote>
 struct TextReader<T, Open, Close, Quote,
     typename std::enable_if<is_stl_set<T>::value>::type> {
   void operator()(std::istream& is, T& t) const {
-		auto die_unless = [](std::istream& is, char c) { 
-			if ( is.get() != c ) {
-				is.setstate(std::ios::failbit);
-			}
-		};
+    auto die_unless = [](std::istream & is, char c) {
+      if (is.get() != c) {
+        is.setstate(std::ios::failbit);
+      }
+    };
 
-		die_unless(is, Open);
-		die_unless(is, ' ');
+    die_unless(is, Open);
+    die_unless(is, ' ');
 
     t.clear();
     while (is.peek() != Close) {
@@ -127,9 +127,9 @@ struct TextReader<T, Open, Close, Quote,
       r(is, v);
       t.insert(v);
 
-			die_unless(is, ' ');
+      die_unless(is, ' ');
     }
-		die_unless(is, Close);
+    die_unless(is, Close);
   }
 };
 
@@ -137,14 +137,14 @@ template <typename T, char Open, char Close, char Quote>
 struct TextReader<T, Open, Close, Quote,
     typename std::enable_if<is_stl_map<T>::value>::type> {
   void operator()(std::istream& is, T& t) const {
-		auto die_unless = [](std::istream& is, char c) { 
-			if ( is.get() != c ) {
-				is.setstate(std::ios::failbit);
-			}
-		};
+    auto die_unless = [](std::istream & is, char c) {
+      if (is.get() != c) {
+        is.setstate(std::ios::failbit);
+      }
+    };
 
-		die_unless(is, Open);
-		die_unless(is, ' ');
+    die_unless(is, Open);
+    die_unless(is, ' ');
 
     t.clear();
     while (is.peek() != Close) {
@@ -153,9 +153,9 @@ struct TextReader<T, Open, Close, Quote,
       r(is, v);
       t.insert(v);
 
-			die_unless(is, ' ');
+      die_unless(is, ' ');
     }
-		die_unless(is, Close);
+    die_unless(is, Close);
   }
 };
 
