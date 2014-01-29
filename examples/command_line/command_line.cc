b13e31ef0d76dfccf1fe377e51da7aab5307b57d
eric <eschkufz@mrwhite.Stanford.EDU>
2014-01-28 18:50:25 -0800

    Testing.

--- examples/command_line/command_line.cc
+++ examples/command_line/command_line.cc
@@ -33,12 +33,12 @@ auto& s = ValueArg<string>::create("s")
           .usage("\"...\"")
           .default_val("Hello, world");
 
-auto& v = ValueArg<vector<int>, RangeReader<vector<int>, 1, 10>>::create("v")
+auto& v = ValueArg<vector<int>, RangeReader<vector<int>,1,10>>::create("v")
           .alternate("vector")
           .usage("{ 1 2 3 }")
           .default_val({1, 2});
 
-auto& set = ValueArg<set<char>, RangeReader<set<char>, 'a', 'z'>>::create("set")
+auto& set = ValueArg<set<char>, RangeReader<set<char>,'a','z'>>::create("set")
             .usage("{ a b c }")
             .default_val({'a', 'b'});
 
