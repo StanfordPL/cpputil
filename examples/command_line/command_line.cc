80fc510a3ad8ad35290a157a79281fb4b1bd7867
eric <eric.schkufza@gmail.com>
2014-02-02 14:08:54 -0800

    Don't like astyle's formatting for this file.

--- examples/command_line/command_line.cc
+++ examples/command_line/command_line.cc
@@ -28,39 +28,39 @@ using namespace std;
 auto& heading = Heading::create("Program-specific arguments:");
 
 auto& i = ValueArg<int, RangeReader<int, Range<int, 1, 10>>>::create("i")
-          .alternate("int")
-          .usage("<int>")
-          .description("i | 1 <= i <= 10")
-          .default_val(5);
+  .alternate("int")
+  .usage("<int>")
+  .description("i | 1 <= i <= 10")
+  .default_val(5);
 
 auto& h = ValueArg<uint64_t, HexReader<uint64_t>, HexWriter<uint64_t>>::create("j")
-          .alternate("hex")
-          .usage("<hex>")
-          .description("A 16 digit hex value, grouped 8 digits at a time. ie: 01234567 89abcdef")
-          .default_val(0);
+  .alternate("hex")
+  .usage("<hex>")
+  .description("A 16 digit hex value, grouped 8 digits at a time. ie: 01234567 89abcdef")
+  .default_val(0);
 
 auto& s = ValueArg<string>::create("s")
-          .alternate("str")
-          .usage("\"...\"")
-          .default_val("Hello, world");
+  .alternate("str")
+  .usage("\"...\"")
+  .default_val("Hello, world");
 
 auto& v = ValueArg<vector<int>, SpanReader<vector<int>, Range<int, 1, 10>>>::create("v")
-          .alternate("vector")
-          .usage("{ 1 2 3 }")
-          .description("A sequence of integers, each between 1 and 10, inclusive")
-          .default_val({1, 2, 3});
+  .alternate("vector")
+  .usage("{ 1 2 3 }")
+  .description("A sequence of integers, each between 1 and 10, inclusive")
+  .default_val({1, 2, 3});
 
 auto& sc = ValueArg<set<char>, SpanReader<set<char>, Range<char, 'a', 'z'>>>::create("sc")
-           .usage("{ a b c }")
-           .description("A set of characters, each between a and z, inclusive. May include ellipses as shorthand. ie: { a ... d }")
-           .default_val({'a', 'b', 'c'});
+  .usage("{ a b c }")
+  .description("A set of characters, each between a and z, inclusive. May include ellipses as shorthand. ie: { a ... d }")
+  .default_val({'a', 'b', 'c'});
 
 auto& vh = FileArg<vector<int>,
       SpanReader<vector<int>, Range<int, 0, 32>, TextStyle<false>>,
       TextWriter<vector<int>, TextStyle<false>>>::create("vh")
-      .usage("/path/to/file.dat")
-      .description("A sequence of 8 digit hex values. May include ellipses as shorthand. ie:{ 00000000 00000001 ... }")
-      .default_val({0, 1, 2});
+  .usage("/path/to/file.dat")
+  .description("A sequence of 8 digit hex values. May include ellipses as shorthand. ie:{ 00000000 00000001 ... }")
+  .default_val({0, 1, 2});
 
 int main(int argc, char** argv) {
   CommandLineConfig::strict_with_convenience(argc, argv);
